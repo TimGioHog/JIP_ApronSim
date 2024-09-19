1baca621f6d87e4d2808281242931771cb31042e
TimGioHog <62881743+TimGioHog@users.noreply.github.com>
2024-09-19 16:41:01 +0200

    Pause Menu, Bridge animation

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ class Operation:
         self.time_left = duration
         self.locations = []
 
+    def reset(self):
+        self.completed = False
+        self.completion_time = None
+        self.start_time = None
+        self.time_left = self.duration
+
     def add_dependency(self, operation):
         self.dependencies.append(operation)
 
@@ -40,20 +46,25 @@ class Operation:
 
 class Scheduler:
     def __init__(self, df):
-        self.operations = {}
+        self.ops = {}
         for index, row in df.iterrows():  # for each operation:
             operation = Operation(row.iloc[0], row.iloc[1] * 60)
             for dep in row.iloc[2:5]:
                 if pd.notna(dep):
-                    operation.add_dependency(self.operations[dep])
+                    operation.add_dependency(self.ops[dep])
             for i in range(3):
                 if pd.notna(row.iloc[2 * i + 5]) and pd.notna(row.iloc[2 * i + 6]):
                     operation.locations.append((row.iloc[2 * i + 5], row.iloc[2 * i + 6]))
-            self.operations[row.iloc[0]] = operation
+            self.ops[row.iloc[0]] = operation
         self.finished = False
 
+    def reset(self):
+        for operation in self.ops.values():
+            self.finished = False
+            operation.reset()
+
     def update(self, sim, duration):
-        for operation in self.operations.values():
+        for operation in self.ops.values():
             if operation.is_ready() and not operation.completed:
                 if operation.start_time is None:
                     operation.start_time = sim.timer
@@ -62,7 +73,7 @@ class Scheduler:
                     operation.completed = True
                     operation.completion_time = sim.timer
                     print(f'{operation} operation completed at time {sim.timer}!')
-        if all(op.completed for op in self.operations.values()):
+        if all(op.completed for op in self.ops.values()):
             self.finished = True
 
 
@@ -77,25 +88,49 @@ class Simulation:
         self.speed = 1
         self.paused = False
         self.pause_menu = False
+        self.running = True
+        self.restart = False
+
+        self.button_resume = Button("RESUME", (820, 280), (280, 50), self.button_resume_action)
+        self.button_quit = Button("QUIT", (820, 730), (280, 50), self.button_quit_action)
+        self.button_restart = Button("RESTART", (820, 360), (280, 50), self.button_restart_action)
 
     def draw(self):
         self.screen.fill('Black')
         self.screen.blit(self.images['apron'], self.rects['apron'])
-        if not self.scheduler.operations["Parking"].completed:
+
+        # Aircraft rendering
+        if not self.scheduler.ops["Parking"].completed:
             self.screen.blit(self.images['737s'], (513, 17 - 1020 + 17 * self.timer))  # 17 pixels per second
-        elif self.scheduler.operations["Pushback"].is_ready():
-            self.screen.blit(self.images['737s'], (513, 17 - 17 * (self.timer - self.scheduler.operations["Pushback"].start_time)))  # 17 pixels per second
+        elif self.scheduler.ops["Pushback"].is_ready():
+            self.screen.blit(self.images['737s'], (513, 17 - (17 / (self.scheduler.ops["Pushback"].duration / 60)) * (self.timer - self.scheduler.ops["Pushback"].start_time)))
         else:
             self.screen.blit(self.images['737s'], (513, 17))
 
+        # Bridge rendering
+        self.screen.blit(self.images['Bridge_1'], (1330, 924))
+        if self.scheduler.ops["Connect_Bridge"].start_time is None or self.scheduler.ops["Remove_Bridge"].completed:
+            self.screen.blit(self.images['Bridge_3'], (1233, 896))
+        elif self.scheduler.ops["Connect_Bridge"].completed and self.scheduler.ops["Remove_Bridge"].start_time is None:
+            self.screen.blit(self.images['Bridge_3'], (987, 854))
+        elif self.scheduler.ops["Remove_Bridge"].start_time is not None:
+            removing_bridge_time = self.timer - self.scheduler.ops["Remove_Bridge"].start_time
+            self.screen.blit(self.images['Bridge_3'], (987 + (((1233 - 987) / self.scheduler.ops["Connect_Bridge"].duration) * removing_bridge_time),
+                                                       854 + (((896 - 854) / self.scheduler.ops["Connect_Bridge"].duration) * removing_bridge_time)))
+        else:
+            connecting_bridge_time = self.timer - self.scheduler.ops["Connect_Bridge"].start_time
+            self.screen.blit(self.images['Bridge_3'], (1233 - (((1233 - 987) / self.scheduler.ops["Connect_Bridge"].duration) * connecting_bridge_time),
+                                                       896 - (((896 - 854) / self.scheduler.ops["Connect_Bridge"].duration) * connecting_bridge_time)))
+
         operation_count = -1
-        for operation in self.scheduler.operations.values():
+        for operation in self.scheduler.ops.values():
             if operation.is_ready() and not operation.completed:
                 operation_count += 1
                 for i in range(len(operation.locations)):
-                    pg.draw.circle(self.screen, (255, 0, 0), operation.locations[i], 10)
-                    self.screen.blit(small_font.render(operation.name, True, (0, 0, 0)),
-                                     (operation.locations[i][0], operation.locations[i][1] + 10))
+                    if operation.name not in ["Parking", "Connect_Bridge", "Remove_Bridge"]:
+                        pg.draw.circle(self.screen, (255, 0, 0), operation.locations[i], 10)
+                        self.screen.blit(small_font.render(operation.name, True, (0, 0, 0)),
+                                         (operation.locations[i][0], operation.locations[i][1] + 10))
                 self.screen.blit(small_font.render(f'{operation}', True, white), (10, 100 + operation_count*35))
 
         pg.draw.rect(self.screen, black, pg.Rect(0, 0, 100, 100))
@@ -118,13 +153,25 @@ class Simulation:
         # FPS Counter
         self.screen.blit(small_font.render(f'{int(self.fps)}', True, white), (1880, 10))
 
+        # Paused Pop-Up
         if self.paused and not self.pause_menu:
             pg.draw.rect(self.screen, black, pg.Rect(816, 0, 288, 60))
             self.screen.blit(large_font.render(f'Simulation Paused', True, white), (826, 10))
-        elif self.pause_menu:
-            rect_surface = pg.Surface((288, 600), pg.SRCALPHA)
+
+        # Pause Menu
+        elif self.pause_menu or self.scheduler.finished:
+            rect_surface = pg.Surface((320, 600), pg.SRCALPHA)
             rect_surface.fill(pg.Color(0, 0, 0, 150))
-            self.screen.blit(rect_surface, (816, 200))
+            self.screen.blit(rect_surface, (800, 200))
+
+            if self.scheduler.finished:
+                self.screen.blit(large_font.render(f'Simulation Finished', True, white), (960 - large_font.size('Simulation Finished')[0] / 2, 220))
+            else:
+                self.screen.blit(large_font.render(f'Simulation Paused', True, white), (960 - large_font.size('Simulation Paused')[0]/2, 220))
+                self.button_resume.draw(self.screen)
+
+            self.button_restart.draw(self.screen)
+            self.button_quit.draw(self.screen)
 
         pg.display.flip()
 
@@ -141,6 +188,11 @@ class Simulation:
                     self.speed = int(self.speed * 2)
                 elif event.unicode == "-" and self.speed >= 2:
                     self.speed = int(self.speed / 2)
+            elif event.type == pg.MOUSEBUTTONUP or event.type == pg.MOUSEBUTTONDOWN or event.type == pg.MOUSEMOTION:
+                if self.pause_menu or self.scheduler.finished:
+                    self.button_quit.handle_event(event)
+                    self.button_restart.handle_event(event)
+                    self.button_resume.handle_event(event)
 
     def update(self, duration):
         self.timer += duration * self.speed
@@ -151,11 +203,8 @@ class Simulation:
         pg.display.set_caption("ApronSim")
         print("Running...")
         last_frame = time.perf_counter()
-        running = True
-        # for image in self.images:
-        #     print(image)
-        #     self.images[image] = pg.image.load(f'assets/{image}.png').convert_alpha()
-        while running:
+
+        while self.running:
             self.event_handler()
             self.draw()
 
@@ -163,10 +212,59 @@ class Simulation:
             frame_duration = current_time - last_frame
             last_frame = current_time
 
-            if not self.paused and not self.pause_menu:
+            if not self.paused and not self.pause_menu and not self.scheduler.finished:
                 self.update(frame_duration)
 
             self.fps = 1 / frame_duration
+
+            if self.restart:
+                print(f'\n Restarting... \n')
+                self.timer = 0
+                self.speed = 1
+                self.paused = False
+                self.pause_menu = False
+                self.scheduler.reset()
+                self.restart = False
+        pg.quit()
+
+    def button_resume_action(self):
+        if not self.scheduler.finished:
+            self.pause_menu = False
+
+    def button_restart_action(self):
+        self.restart = True
+
+    def button_quit_action(self):
+        self.running = False
+
+
+class Button:
+    def __init__(self, text, pos, size, callback, color=(200, 200, 200), hover_color=klm_rgb, font_size=40):
+        self.text = text
+        self.pos = pos
+        self.size = size
+        self.color = color
+        self.hover_color = hover_color
+        self.callback = callback
+        self.rect = pg.Rect(pos, size)
+        self.font = pg.font.Font(None, font_size)
+        self.is_hovered = False
+        self.text_pos = (self.rect.x + size[0]/2 - self.font.size(text)[0]/2, self.rect.y + size[1]/2 - self.font.size(text)[1]/2)
+
+    def draw(self, screen):
+        # Change color on hover
+        current_color = self.hover_color if self.is_hovered else self.color
+        pg.draw.rect(screen, current_color, self.rect)
+
+        # Render text
+        text_surface = self.font.render(self.text, True, black)
+        screen.blit(text_surface, (self.text_pos[0], self.text_pos[1]))
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEMOTION:
+            self.is_hovered = self.rect.collidepoint(event.pos)
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and self.is_hovered:
+            self.callback()
 
 
 def load_assets():
